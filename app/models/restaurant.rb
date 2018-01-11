886f0b6b2516e03230299b667976921879fa248f
Raghu Betina <raghu@firstdraft.com>
2018-01-11 20:25:56 +0000

    Direct association between neighborhoods and restaurants

--- app/models/restaurant.rb
+++ app/models/restaurant.rb
@@ -1,6 +1,10 @@
 class Restaurant < ApplicationRecord
   # Direct associations
 
+  belongs_to :neighborhood,
+             :required => false,
+             :counter_cache => true
+
   has_many   :bookmarks,
              :dependent => :destroy
 
